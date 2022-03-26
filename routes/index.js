e3d46a1938469edd6b6b71cab854736617e15018
Guilherme Queiróz <g.aqueiroz19@gmail.com>
2022-03-26 02:09:30 -0300

    testing send json

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send({
+    name: 'Guilherme'
+  })
 })
 
 module.exports = router
