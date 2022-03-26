8eda18cb9d0047fac7287a17951e23763f390b5e
Guilherme Queiróz <g.aqueiroz19@gmail.com>
2022-03-26 01:25:53 -0300

    initial setup

---
+++ server.js
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+app.listen(process.env.PORT || 3000)
+// app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Mongoose is successfully connected'))
+
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
+
